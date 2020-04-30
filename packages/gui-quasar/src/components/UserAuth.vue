ca24d7edf8b0a20d4e7135516a6f458a20b38227
Zaephor <Zaephor@users.noreply.github.com>
2020-04-29 22:06:31 -0700

    refactor(gui-quasar): seperate out login and register paths

--- packages/gui-quasar/src/components/UserAuth.vue
+++ packages/gui-quasar/src/components/UserAuth.vue
@@ -1,18 +1,18 @@
 <!-- Should Cover UserLogin/UserRegistration and User's Own Profile Management -->
 <template>
   <div>
-    <!--  Registration and login section  -->
+    <!-- Login -->
     <q-expansion-item
       expand-separator
-      icon="create"
-      label="Register/Login"
+      icon="lock"
+      label="Login"
       v-if="gglass.user === false"
     >
       <q-card>
         <q-card-section>
           <q-form
             @submit="userLogin"
-            @reset="userReset"
+            @reset="login = { email: null, password: null }"
             class="q-gutter-sm"
             ref="loginForm"
           >
@@ -20,8 +20,7 @@
               filled
               dense
               bottom-slots
-              name="userEmail"
-              v-model="userEmail"
+              v-model="login.email"
               label="Email"
               :rules="[
                 (val) => (val && val.length > 0) || 'Please enter something.',
@@ -33,10 +32,9 @@
             <q-input
               filled
               dense
-              name="userPassword"
-              v-model="userPassword"
-              :type="isPwd ? 'password' : 'text'"
+              v-model="login.password"
               label="Password"
+              :type="isPwd ? 'password' : 'text'"
               :rules="[
                 (val) =>
                   (val && val.length >= 8) ||
@@ -61,20 +59,73 @@
                   size="sm"
                   tabindex="-1"
                 />
+                <q-btn push label="Login" type="submit" size="sm" />
+              </q-btn-group>
+            </div>
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+
+    <!-- Register -->
+    <q-expansion-item
+      expand-separator
+      icon="create"
+      label="Register"
+      v-if="gglass.user === false"
+    >
+      <q-card>
+        <q-card-section>
+          <q-form
+            @submit="userRegister"
+            @reset="register = { email: null, password: null }"
+            class="q-gutter-sm"
+            ref="loginForm"
+          >
+            <q-input
+              filled
+              dense
+              bottom-slots
+              v-model="register.email"
+              label="Email"
+              :rules="[
+                (val) => (val && val.length > 0) || 'Please enter something.',
+                (val) =>
+                  /\S+@\S+\.\S+/.test(val) || 'Please enter a valid email.',
+              ]"
+            />
+
+            <q-input
+              filled
+              dense
+              v-model="register.password"
+              label="Password"
+              :type="isPwd ? 'password' : 'text'"
+              :rules="[
+                (val) =>
+                  (val && val.length >= 8) ||
+                  'Please use a password longer than 8 characters',
+              ]"
+            >
+              <template v-slot:append>
+                <q-icon
+                  :name="isPwd ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="isPwd = !isPwd"
+                />
+              </template>
+            </q-input>
+
+            <div align="right">
+              <q-btn-group push>
                 <q-btn
                   push
-                  label="Register"
-                  v-on:click="userRegister"
+                  label="Reset"
+                  type="reset"
                   size="sm"
                   tabindex="-1"
                 />
-                <q-btn
-                  push
-                  label="Login"
-                  type="submit"
-                  name="login"
-                  size="sm"
-                />
+                <q-btn push label="Register" type="submit" size="sm" />
               </q-btn-group>
             </div>
           </q-form>
@@ -84,7 +135,12 @@
 
     <!--  User Profile/etc section  -->
     <template v-if="gglass.user !== false">
-      <q-expansion-item expand-separator icon="face" label="User Profile">
+      <q-expansion-item
+        expand-separator
+        icon="face"
+        label="User Profile"
+        @before-show="gglassWhoami"
+      >
         <q-card>
           <q-card-section>
             {{ gglass.user.id }}
@@ -117,14 +173,19 @@ export default {
   data() {
     return {
       isPwd: true,
-      userEmail: null,
-      userPassword: null,
+      login: {
+        email: null,
+        password: null,
+      },
+      register: {
+        email: null,
+        password: null,
+      },
     };
   },
   methods: {
     ...mapActions("gglass", [
       "toggleGlassNav", // -> this.toggleGlassNav()
-      "setNavTab",
       "gglassRegister",
       "gglassLogin",
       "gglassWhoami",
@@ -134,29 +195,25 @@ export default {
       let valid = await this.$refs.loginForm.validate();
       if (valid) {
         await this.gglassRegister({
-          email: this.userEmail,
-          password: this.userPassword,
+          email: this.register.email,
+          password: this.register.password,
         });
 
         if (this.gglass.user !== false) {
-          this.userEmail = null;
+          this.register.email = null;
         }
-        this.userPassword = null;
+        this.register.password = null;
       }
     },
     userLogin: async function () {
       await this.gglassLogin({
-        email: this.userEmail,
-        password: this.userPassword,
+        email: this.login.email,
+        password: this.login.password,
       });
       if (this.gglass.user !== false) {
-        this.userEmail = null;
+        this.login.email = null;
       }
-      this.userPassword = null;
-    },
-    userReset: function () {
-      this.userEmail = null;
-      this.userPassword = null;
+      this.login.password = null;
     },
   },
   created: function () {
